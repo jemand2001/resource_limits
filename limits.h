ae52e0b6cd4bcc162eab7ace1430cce1c408e464
jemand2001 <jmand@gmx.de>
2020-12-01 00:49:45 +0100

    missed some resources

--- limits.h
+++ limits.h
@@ -3,7 +3,7 @@
 
 #include <sys/resource.h>
 
-#define ARRAY_ENTRY(name, x, desc) {#name, desc, x},
+#define ARRAY_ENTRY(name, desc) {#name, desc, name},
 
 struct limit {
     const char *name, *desc;
@@ -11,12 +11,22 @@ struct limit {
 };
 
 #define LIMITS(X)\
-    X(RLIMIT_CPU,    0, "Per-process CPU limit, in seconds.")\
-    X(RLIMIT_FSIZE,  1, "Largest file that can be created, in bytes.")\
-    X(RLIMIT_DATA,   2, "Maximum size of data segment, in bytes.")\
-    X(RLIMIT_STACK,  3, "Maximum size of stack segment, in bytes.")\
-    X(RLIMIT_CORE,   4, "Largest core file that can be created, in bytes.")\
-    X(RLIMIT_NOFILE, 7, "Number of open files.")\
-    X(RLIMIT_AS,     9, "Address space limit.")
+    X(RLIMIT_CPU,        "Per-process CPU limit, in seconds.")\
+    X(RLIMIT_FSIZE,      "Largest file that can be created, in bytes.")\
+    X(RLIMIT_DATA,       "Maximum size of data segment, in bytes.")\
+    X(RLIMIT_STACK,      "Maximum size of stack segment, in bytes.")\
+    X(RLIMIT_CORE,       "Largest core file that can be created, in bytes.") \
+    X(RLIMIT_RSS,        "Largest resident set size, in bytes.")             \
+    X(RLIMIT_NPROC,      "Number of processes.")             \
+    X(RLIMIT_NOFILE,     "Number of open files.")             \
+    X(RLIMIT_MEMLOCK,    "Locked-in-memory address space.")             \
+    X(RLIMIT_AS,         "Address space limit.")              \
+    X(RLIMIT_LOCKS,      "Maximum number of file locks.")\
+    X(RLIMIT_SIGPENDING, "Maximum number of pending signals.")\
+    X(RLIMIT_MSGQUEUE,   "Maximum bytes in POSIX message queues.")\
+    X(RLIMIT_NICE,       "Maximum nice priority allowed to raise to.")\
+    X(RLIMIT_RTPRIO,     "Maximum realtime priority allowed for non-priviledged processes.")\
+    X(RLIMIT_RTTIME,     "Maximum CPU time in µs that a process scheduled under a real-time"\
+                         " scheduling policy may consume at once.")
 
 #endif //RESOURCE_LIMITS_LIMITS_H
