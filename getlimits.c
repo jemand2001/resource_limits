c66ca51d6ced239814c0347a10e9ed4ed2abdcf9
jemand2001 <jmand@gmx.de>
2020-12-01 00:50:01 +0100

    better formatting

--- getlimits.c
+++ getlimits.c
@@ -5,6 +5,7 @@
 
 #include "limits.h"
 
+int max_name();
 struct limit limits[] = {
         LIMITS(ARRAY_ENTRY)
 };
@@ -13,10 +14,20 @@ size_t limits_num = sizeof(limits) / sizeof(struct limit);
 
 int main() {
     struct rlimit the_limit;
-    printf("%-15s\t%-20s\t%-20s\tdescription\n", "resource name", "soft limit", "hard limit");
+    int name_cols = max_name();
+    printf("%-*s | %-20s | %-20s | description\n", name_cols, "resource name", "soft limit", "hard limit");
     for (int i = 0; i < limits_num; i++) {
         getrlimit(limits[i].num, &the_limit);
-        printf("%-15s\t%20lu\t%20lu\t%s\n", limits[i].name, the_limit.rlim_cur, the_limit.rlim_max, limits[i].desc);
+        printf("%-*s | %20lu | %20lu | %s\n", name_cols, limits[i].name, the_limit.rlim_cur, the_limit.rlim_max, limits[i].desc);
     }
     return 0;
+}
+
+int max_name() {
+    int res = 0, new_res;
+    for (int i = 0; i < limits_num; i++) {
+        if ((new_res = (int) strlen(limits[i].name)) > res)
+            res = new_res;
+    }
+    return res;
 }
